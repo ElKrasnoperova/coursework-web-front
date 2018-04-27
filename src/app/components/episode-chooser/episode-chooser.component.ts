f838382f23bb43ee16e80f9ad51e2b5ce57dcd2b
nvmb5f <nvmb5f@gmail.com>
2018-04-28 01:20:44 +0300

    episode-chooser !

--- src/app/components/episode-chooser/episode-chooser.component.ts
+++ src/app/components/episode-chooser/episode-chooser.component.ts
@@ -1,8 +1,9 @@
 // TODO Change color of selected item
 // TODO Make component adaptive
 
-
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Episode} from '../../model/Episode';
+import {EpisodeService} from '../../service/episode.service';
 
 @Component({
   selector: 'app-episode-chooser',
@@ -10,31 +11,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./episode-chooser.component.css']
 })
 export class EpisodeChooserComponent implements OnInit {
-  checked = false;
-  indeterminate = false;
-  align = 'start';
-  episodeDisabled = true;
-  seasonDisabled = true;
-  selectedSeason = null;
-  selectedEpisode = null;
-  constructor() { }
+  seasons: Episode[];
+  episodes: Episode[];
+
+  selectedEpisode: Episode;
+
+  @Output()
+  episodeSelected: EventEmitter<Episode> = new EventEmitter<Episode>();
 
   ngOnInit() {
+    this.getSeasons();
   }
 
-  enableEpisodeSelect() {
-    this.episodeDisabled = false;
+  constructor(private episodeService: EpisodeService) {
+    this.selectedEpisode = new Episode();
   }
 
-  enableChronology(state) {
-    if (state === true) {
-      if (this.selectedSeason !== null) {
-        this.episodeDisabled = false;
-      }
-      this.seasonDisabled = false;
-    } else {
-      this.seasonDisabled = true;
-      this.episodeDisabled = true;
-    }
+  getSeasons() {
+    this.episodeService.getSeasons()
+      .then( items => {
+        this.seasons = items;
+      });
   }
+
+  getEpisodesForSeason(season: Episode) {
+    this.episodeService.getEpisodesForSeasons(season.seasonNumber)
+      .then(items => {
+        this.episodes = items;
+      });
+  }
+
+  setEpisode(value: any) {
+    this.selectedEpisode = value;
+    this.episodeSelected.emit(value);
+  }
+
+  // checked = false;
+  // indeterminate = false;
+  // align = 'start';
+  // episodeDisabled = true;
+  // seasonDisabled = true;
+  // selectedSeason = null;
+  // selectedEpisode = null;
+  //
+  // enableEpisodeSelect() {
+  //   this.episodeDisabled = false;
+  // }
+  //
+  // enableChronology(state) {
+  //   if (state === true) {
+  //     if (this.selectedSeason !== null) {
+  //       this.episodeDisabled = false;
+  //     }
+  //     this.seasonDisabled = false;
+  //   } else {
+  //     this.seasonDisabled = true;
+  //     this.episodeDisabled = true;
+  //   }
+  // }
 }
